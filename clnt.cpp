54723713c40d5492e803b3dd465d46bbf12ee619
wuchenhi <wuyi@DESKTOP-IMODEMS.localdomain>
2022-03-24 23:30:21 +0800

    first

---
+++ clnt.cpp
@@ -0,0 +1,58 @@
+#include <fstream>
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include <string>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>  
+
+using std::string;
+using std::ifstream;
+
+const int BufSize = 100;
+
+void error_handling(string message);
+
+int main(int argc, char *argv[])
+{
+
+    int sd;
+    
+    FILE *fp;  //TODO
+
+    char buf[BufSize];
+    int read_cnt;
+    struct sockaddr_in serv_adr;
+
+    if (argc != 3)
+    {
+        std::cout<<"Please use "<< argv[0] <<" <IP> <port>"<<std::endl;
+        exit(1);
+    }
+
+    fp = fopen("receive.cpp", "wb");
+    sd = socket(PF_INET, SOCK_STREAM, 0);
+
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_adr.sin_port = htons(atoi(argv[2]));
+
+    connect(sd, (struct sockaddr *)&serv_adr, sizeof(serv_adr));
+
+    while ((read_cnt = read(sd, buf, BufSize)) != 0)
+        fwrite((void *)buf, 1, read_cnt, fp);
+
+    std::cout<< "Received file data" <<std::endl;
+    write(sd, "Thank you", 10);
+    fclose(fp);
+    close(sd);
+    return 0;
+}
+
+void error_handling(string message){
+    std::cerr << message << std::endl;
+    exit(1);
+}
---
+++ server.cpp
@@ -0,0 +1,71 @@
+#include <fstream>
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include <string>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>  
+
+using std::string;
+using std::ifstream;
+
+const int BufSize = 100;
+
+void erro_handling(string message);
+
+int main(int argc, char *argv[])
+{
+    //服务端、客户端文件描述符
+    int serv_sd, clnt_sd;
+
+    char buf[BufSize];
+
+    struct sockaddr_in serv_adr, clnt_adr;
+    socklen_t clnt_adr_sz;
+    //若未指定端口，返回
+    if(argc != 2){
+        std::cout<<"Please use "<< argv[0] <<" <port>"<<std::endl;
+        exit(1);
+    }
+    //读文件
+    ifstream ifstrm("testWord",ifstream::in);
+    serv_sd = socket(PF_INET, SOCK_STREAM, 0);
+
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_adr.sin_port = htons(atoi(argv[1]));
+
+    bind(serv_sd, (struct sockaddr *)&serv_adr, sizeof(serv_adr));
+
+    listen(serv_sd, 5);
+
+    clnt_adr_sz = sizeof(clnt_adr);
+    clnt_sd = accept(serv_sd, (struct sockaddr *)&clnt_adr, &clnt_adr_sz);
+
+    if (!ifstrm.is_open())
+    { 
+        std::cout << "Error opening file" << std::endl;
+        exit (1); 
+    }  
+
+    while (!ifstrm.eof())
+    {
+        ifstrm.getline(buf, BufSize);
+        write(clnt_sd, buf, BufSize);
+    }      
+    shutdown(clnt_sd, SHUT_WR);
+    read(clnt_sd, buf, BufSize);
+
+    ifstrm.close();
+    close(clnt_sd);
+    close(serv_sd);
+    return 0;    
+}
+
+void error_handling(string message){
+    std::cerr << message << std::endl;
+    exit(1);
+}
