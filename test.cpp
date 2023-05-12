faca198a5fc2982a2201ff6e02ab0787eff5fb78
wuchenhi <1634980701@qq.com>
2023-05-12 18:36:36 +0800

    update

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include <iostream>
-
-int main() {
-    char* text = "get /"; 
-    char* m_url = strpbrk(text," \t");
-    std::cout << text;
-    std::cout << strcasecmp("h\01", "h");
-}
